16e5b8307d5fa84f80eb7c7804b1b4e548aeb936
bgfxc4 <bindermarc@gmx.de>
2022-09-05 02:20:15 +0200

    update manifest and remove unused import

--- hass_pc_monitor/monitorConnection.py
+++ hass_pc_monitor/monitorConnection.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.update_coordinator import (DataUpdateCoordinator, UpdateFailed)
+from homeassistant.helpers.update_coordinator import (DataUpdateCoordinator)
 
 import collections
 import socket
